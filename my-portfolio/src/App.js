78d864c7de7f8bea1831728d506fbef93e1a492f
finnickyfinn <finncastell04@gmail.com>
2023-10-02 15:42:39 -0400

    Moved into new repo to fix issues

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+
+
+
+
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+export default function App() {
+  return "App";
+}
